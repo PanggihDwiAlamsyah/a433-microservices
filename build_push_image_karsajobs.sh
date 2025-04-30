e176959941eadbd9c13f103e107fc5865b3f547d
PanggihDwiAlamsyah <pdwialamsyah@gmail.com>
2025-04-30 13:48:38 +0700

    fix cirleci BE

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,35 +1,17 @@
 #!/bin/bash
 
 # username GitHub dan nama image
-GITHUB_USER="${GITHUB_USERNAME}"
+GITHUB_USER="panggihdwialamsyah"
 IMAGE_NAME="ghcr.io/$GITHUB_USER/karsajobs"
 
 # Build Docker image dari Dockerfile di direktori saat ini
 docker build -t $IMAGE_NAME:latest .
 
-# Login ke GitHub Container Registry menggunakan Personal Access Token (CR_PAT)
-echo $CR_PAT | docker login ghcr.io -u $GITHUB_USER --password-stdin
+# Login ke GitHub Container Registry menggunakan Personal Access Token (GITHUB_TOKEN)
+echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin
 
 # Push image yang sudah dibuild ke GHCR
 docker push $IMAGE_NAME:latest
 
 # Menampilkan info
 echo "Image pushed to GitHub Packages: $IMAGE_NAME:latest"
-
-
-
-# # username GitHub dan nama image
-# GITHUB_USER="panggihdwialamsyah"
-# IMAGE_NAME="ghcr.io/$GITHUB_USER/karsajobs"
-
-# # Build Docker image dari Dockerfile di direktori saat ini
-# docker build -t $IMAGE_NAME:latest .
-
-# # Login ke GitHub Container Registry menggunakan Personal Access Token (GITHUB_TOKEN)
-# echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin
-
-# # Push image yang sudah dibuild ke GHCR
-# docker push $IMAGE_NAME:latest
-
-# # Menampilkan info
-# echo "Image pushed to GitHub Packages: $IMAGE_NAME:latest"
